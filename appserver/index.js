249e173bfe33d94be340f0fdb916377288658840
Josh Whiting <joshwhiting@gmail.com>
2023-03-27 20:14:11 -0700

    reenable prisma logic

--- appserver/index.js
+++ appserver/index.js
@@ -1,24 +1,23 @@
 const express = require('express');
 const app = express();
 const escapeHtml = require('escape-html')
-// const { PrismaClient } = require('@prisma/client');
+const { PrismaClient } = require('@prisma/client');
 
-// const prisma = new PrismaClient();
+const prisma = new PrismaClient();
 
 app.get('/', async (req, res) => {
   console.log(`Received a request.`);
   
   let queryResult
-  // if (process.env.FLAG_USE_DB === 'true') {
-  //   try {
-  //     queryResult =  await prisma.DummyRecord.findMany()
-  //   } catch(e) {
-  //     queryResult = {"error":e,"message":e.message}
-  //   }
-  // } else {
-  //   queryResult = "db flag is disabled"
-  // }
-  queryResult = "prisma problems"
+  if (process.env.FLAG_USE_DB === 'true') {
+    try {
+      queryResult = await prisma.DummyRecord.findMany()
+    } catch(e) {
+      queryResult = {"error":e,"message":e.message}
+    }
+  } else {
+    queryResult = "FLAG_USE_DB is false"
+  }
 
   res.send(
     `<html><body>
