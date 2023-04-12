737d3f9912e549f886d2a3b3193eea3b9f78c3b8
Josh Whiting <joshwhiting@gmail.com>
2023-04-11 23:33:44 -0700

    somethings up with typescript in knex migrations/seeds, use plain js for now

--- appserver/seeds/dummy_seed.ts
+++ appserver/seeds/dummy_seed.ts
@@ -1,13 +1,13 @@
-import { Knex } from "knex";
-
-export async function seed(knex: Knex): Promise<void> {
+exports.seed = function (knex) {
   // Deletes ALL existing entries
-  await knex("dummy_records").del();
-
-  // Inserts seed entries
-  await knex("dummy_records").insert([
-    { id: 1, value: "rowValue1" },
-    { id: 2, value: "rowValue2" },
-    { id: 3, value: "rowValue3" },
-  ]);
-}
+  return knex("dummy_records")
+    .del()
+    .then(() =>
+      // Inserts seed entries
+      knex("dummy_records").insert([
+        { id: 1, value: "rowValue1" },
+        { id: 2, value: "rowValue2" },
+        { id: 3, value: "rowValue3" },
+      ])
+    );
+};
