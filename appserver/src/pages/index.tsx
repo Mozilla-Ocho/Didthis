a290e9ddcbb239fcf66035276a7bf994580c816e
Josh Whiting <joshwhiting@gmail.com>
2023-05-10 13:13:09 -0700

    use 'components' plural for directory name, for consistency, and also matches tailwind default config

--- appserver/src/pages/index.tsx
+++ appserver/src/pages/index.tsx
@@ -1,23 +1,29 @@
 import knex from "@/knex";
-//import Button from '@/component/uiLib/button.tsx';
-import {H,Button,Divider} from '@/component/uiLib';
+import { H, Button, Divider } from "@/components/uiLib";
 
 export default function Home({ dbResult }: { dbResult: string }) {
-  console.log("homepage render")
+  console.log("homepage render");
   return (
     <main className="p-10">
       <div>
         <H.H1>h3y</H.H1>
-        <p><em>psst... it stands for hobby</em></p>
+        <p>
+          <em>psst... it stands for hobby</em>
+        </p>
         <pre>database query result: {dbResult}</pre>
       </div>
-      <Divider/>
+      <Divider />
       <H.H2>vanilla html upload tester</H.H2>
-      <form action='/api/upload' encType='multipart/form-data' method="post">
-        <input id='files' type="file" name="thefile" accept="image/*, video/*" />
-        <Button type='submit'>Upload</Button>
+      <form action="/api/upload" encType="multipart/form-data" method="post">
+        <input
+          id="files"
+          type="file"
+          name="thefile"
+          accept="image/*, video/*"
+        />
+        <Button type="submit">Upload</Button>
       </form>
-      <Divider/>
+      <Divider />
       <H.H3>heading 3</H.H3>
       <H.H4>heading 4</H.H4>
     </main>
@@ -28,8 +34,8 @@ export async function getServerSideProps() {
   let dbResult;
   if (process.env.FLAG_USE_DB === "true") {
     try {
-      dbResult = await knex('dummy_records').first();
-      dbResult = dbResult ? JSON.stringify(dbResult) : 'no rows found';
+      dbResult = await knex("dummy_records").first();
+      dbResult = dbResult ? JSON.stringify(dbResult) : "no rows found";
     } catch (e: unknown) {
       dbResult = `db error: ${String(e)}`;
     }
