44fb03ab2b5da0ea74dc4c0227f069e7bc05adc0
Josh Whiting <joshwhiting@gmail.com>
2023-07-07 09:53:27 -0700

    forgot pv tracking on user edit page

--- appserver/src/components/pages/UserEdit.tsx
+++ appserver/src/components/pages/UserEdit.tsx
@@ -1,9 +1,13 @@
 import UserForm from '@/components/forms/User'
+import { useStore } from '@/lib/store'
+import { trackingEvents } from '@/lib/trackingEvents'
 import { observer } from 'mobx-react-lite'
 import Breadcrumbs from '../Breadcrumbs'
-import {PagePad} from '../uiLib'
+import { PagePad } from '../uiLib'
 
 const UserEditPage = observer(() => {
+  const store = useStore()
+  store.useTrackedPageEvent(trackingEvents.pvEditAccount)
   return (
     <>
       <Breadcrumbs crumbs={[{ name: 'Account details' }]} />
