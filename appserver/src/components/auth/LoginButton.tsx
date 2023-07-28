72da4bb98d4af6170170599a84c8eb075603183c
Josh Whiting <joshwhiting@gmail.com>
2023-07-28 11:46:53 -0600

    unauth homepage updates - text revisions, image updates, CTA button above the fold, etc

--- appserver/src/components/auth/LoginButton.tsx
+++ appserver/src/components/auth/LoginButton.tsx
@@ -1,31 +1,37 @@
 import { Button } from '@/components/uiLib'
 import { observer } from 'mobx-react-lite'
 import { useStore } from '@/lib/store'
-import {trackingEvents} from '@/lib/trackingEvents'
+import { trackingEvents } from '@/lib/trackingEvents'
+import branding from '@/lib/branding'
 
 const LoginButton = observer(
   ({
     intent,
     text,
     'data-testid': dataTestid,
+    className,
   }: {
     overrideCodeCheck?: boolean
     overrideCodeCheckIfNoSignupCode?: boolean
     intent?: React.ComponentProps<typeof Button>['intent']
     text?: string
     'data-testid'?: string
+    className?: string
   }) => {
     const store = useStore()
     const handleClick = () => {
       store.trackEvent(trackingEvents.bcLoginSignup)
       store.launchGlobalLoginOverlay(false)
     }
-    const defaultText = store.signupCodeInfo ? 'Create account' : 'Sign in'
+    const defaultText = store.signupCodeInfo
+      ? branding.signupButtonTxt
+      : branding.loginButtonTxt
     return (
       <Button
         onClick={handleClick}
         intent={intent}
         data-testid={dataTestid || 'loginButton'}
+        className={className}
       >
         {text || defaultText}
       </Button>
