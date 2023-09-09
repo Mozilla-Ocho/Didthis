096a8fa3f733f4b1ef2f5840e634a30cca4d9c5c
Les Orchard <lorchard@mozilla.com>
2023-09-08 17:53:45 -0700

    try building a reducer for the post editor

--- integrations/mobile-app/components/PostEditor.tsx
+++ integrations/mobile-app/components/PostEditor.tsx
@@ -6,19 +6,46 @@ import * as ImagePicker from "expo-image-picker";
 
 import { styles } from "../styles";
 import { ApiPost } from "../store/types";
+import { Post } from "./Post";
 
 export type PostEditorProps = {
   post: ApiPost;
 };
 
-export const PostEditor = ({ post }: PostEditorProps) => {
-  const [image, setImage] = useState<ImagePicker.ImagePickerAsset | null>(null);
-  const [date, setDate] = useState(new Date(1598051730000));
-  const [mode, setMode] = useState("date");
-  const [show, setShow] = useState(false);
-  const [currentDescription, setCurrentDescription] = useState(
-    post.description
-  );
+type PostEditorReducerAction =
+  | {
+      type: "didThisAt";
+      value: ApiPost["didThisAt"];
+    }
+  | {
+      type: "description";
+      value: ApiPost["description"];
+    }
+  | {
+      type: "image";
+      value: ApiPost["image"];
+    };
+
+export function postEditorReducer(
+  state: ApiPost,
+  action: PostEditorReducerAction
+) {
+  switch (action.type) {
+    case "didThisAt":
+      return { ...state, didThisAt: action.value };
+    case "description":
+      return { ...state, description: action.value };
+    case "image":
+      return { ...state, image: action.value };
+    default: {
+      return state;
+    }
+  }
+}
+
+export const PostEditor = ({ post: initialPost }: PostEditorProps) => {
+  const [post, dispatch] = useReducer(postEditorReducer, initialPost);
+  const [showDatePicker, setShowDatePicker] = useState(false);
 
   const pickImage = async () => {
     // No permissions request is necessary for launching the image library
@@ -31,66 +58,45 @@ export const PostEditor = ({ post }: PostEditorProps) => {
       base64: true,
       allowsMultipleSelection: false,
     });
-    if (result.canceled) {
-      return;
+    if (!result.canceled) {
+      dispatch({ type: "image", value: result.assets[0] });
     }
-
-    //setImage(result.assets[0].uri);
-    setImage(result.assets[0]);
   };
 
-  const onChange = (event, selectedDate) => {
-    const currentDate = selectedDate;
-    setShow(false);
-    setDate(currentDate);
+  const onChangeDidThisAt = (_event, selectedDate: Date) => {
+    setShowDatePicker(false);
+    dispatch({ type: "didThisAt", value: new Date(selectedDate).getTime() });
   };
 
-  const showMode = (currentMode) => {
-    setShow(true);
-    setMode(currentMode);
-  };
+  const onChangeDescription = (value: string) =>
+    dispatch({ type: "description", value });
 
   const showDatepicker = () => {
-    showMode("date");
-  };
-
-  const showTimepicker = () => {
-    showMode("time");
-  };
-
-  const onChangeDescription = (newValue) => {
-    setCurrentDescription(newValue)
+    setShowDatePicker(true);
   };
 
   return (
     <>
+      <Post post={post} />
+
       <View style={styles.post}>
-        <Text>{post.id}</Text>
-        <Text>{new Date(post.didThisAt).toLocaleDateString()}</Text>
         <Button onPress={showDatepicker} title="Show date picker!" />
-        <Button onPress={showTimepicker} title="Show time picker!" />
-        <Text>selected: {date.toLocaleString()}</Text>
-        {show && (
+        {showDatePicker && (
           <DateTimePicker
             testID="dateTimePicker"
-            value={date}
-            mode={mode}
-            is24Hour={true}
-            onChange={onChange}
+            value={new Date(post.didThisAt)}
+            mode="datetime"
+            onChange={onChangeDidThisAt}
           />
         )}
         <Button title="Pick an image from camera roll" onPress={pickImage} />
-        {image && <>
-          <Image source={{ uri: 'data:image/jpeg;base64,' + image.base64 }} style={{ width: 200, height: 200 }} />
-          <Text>{JSON.stringify(image.exif)}</Text>
-        </>}
         <TextInput
           onChangeText={onChangeDescription}
-          value={currentDescription}
+          defaultValue={post.description}
+          value={post.description}
           placeholder="post description"
           style={{ padding: 10, borderColor: "#000000", borderWidth: 2 }}
         />
-        {post.description && <Text>{currentDescription}</Text>}
       </View>
     </>
   );
