566aa651e71ef1143c691c9c16287665390e68b8
Les Orchard <lorchard@mozilla.com>
2023-09-12 16:47:08 -0700

    clean up Home.tsx a lil

--- integrations/mobile-app/screens/Home.tsx
+++ integrations/mobile-app/screens/Home.tsx
@@ -1,15 +1,11 @@
 import * as React from "react";
-import { useState, useRef, useEffect } from "react";
+import { useState, useRef } from "react";
 import Constants from "expo-constants";
 import {
-  Button,
-  View,
-  Text,
   SafeAreaView,
   ActivityIndicator,
   StyleSheet,
   StatusBar,
-  TouchableOpacity,
 } from "react-native";
 import type { RootStackParamList } from "../App";
 import type { NativeStackScreenProps } from "@react-navigation/native-stack";
@@ -42,21 +38,6 @@ export default function HomeScreen(props: HomeScreenProps) {
     WebViewNavigation | undefined
   >();
 
-  /*
-  useEffect(() => {
-    navigation.setOptions({
-      headerRight: () => (
-        <Button
-          title="Go to Projects"
-          onPress={() =>
-            navigation.navigate("Projects", { now: Date.now(), thingy: "yup" })
-          }
-        />
-      ),
-    });
-  }, [navigation]);
-  */
-
   const handleWebViewNavigationStateChange = (navState: WebViewNavigation) =>
     setWebviewNavigation(navState);
 
@@ -81,24 +62,6 @@ export default function HomeScreen(props: HomeScreenProps) {
         webview={webviewRef.current}
         webviewNavigation={webviewNavigation}
       />
-      {/*
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text>Home Screen: {JSON.stringify(props)}</Text>
-      <Text>Count: {count}</Text>
-      <Button
-        title="Go to Didthis"
-        onPress={() =>
-          navigation.navigate("DidthisWeb", { now: Date.now(), thingy: "yup" })
-        }
-      />
-      <Button
-        title="Go to Projects"
-        onPress={() =>
-          navigation.navigate("Projects", { now: Date.now(), thingy: "yup" })
-        }
-      />
-    </View>
-      */}
     </SafeAreaView>
   );
 }
