3f9366740c2d7a243678f175f391cb127bb714dc
Les Orchard <lorchard@mozilla.com>
2023-10-09 12:22:35 -0700

    fix app config to make expo main update workflow happier

--- integrations/mobile-app/app.config.js
+++ integrations/mobile-app/app.config.js
@@ -7,18 +7,20 @@ export default ({ config }) => {
     // owner: "lmorchard",
     ios: {
       // bundleIdentifier: "org.mozilla.Didthis"
-      bundleIdentifier: "com.lmorchard.Didthis"
+      bundleIdentifier: "com.lmorchard.Didthis",
     },
     extra: {
       eas: {
         // mozilla-ocho-h3y org project
-        projectId: "d1216396-7cf9-4a41-9543-ce7e31e0529b"
+        projectId: "d1216396-7cf9-4a41-9543-ce7e31e0529b",
         // lmorchard's old sandbox expo project
         // projectId: "bb2e6caf-a447-43b6-86cb-4ee477e6abc3",
       },
     },
     updates: {
-      url: "https://u.expo.dev/bb2e6caf-a447-43b6-86cb-4ee477e6abc3",
+      url: "https://u.expo.dev/d1216396-7cf9-4a41-9543-ce7e31e0529b",
+      // lmorchard's old sandbox expo project
+      // url: "https://u.expo.dev/bb2e6caf-a447-43b6-86cb-4ee477e6abc3",
     },
     runtimeVersion: {
       policy: "sdkVersion",
