5039004e595ba68061a07e0e17e9c3a7d7cd9b2a
Les Orchard <lorchard@mozilla.com>
2023-10-12 12:17:22 -0700

    clean up and switch over to Mozilla identifiers for app

--- integrations/mobile-app/app.config.js
+++ integrations/mobile-app/app.config.js
@@ -5,14 +5,11 @@ export default ({ config }) => {
     slug: "didthis",
     icon: "./assets/didthis-snap-logo.png",
     owner: "mozilla-ocho-h3y",
-    // owner: "lmorchard",
     android: {
-      package: "com.lmorchard.Didthis",
+      package: "org.mozilla.Didthis",
     },
     ios: {
-      //bundleIdentifier: "org.mozilla.Didthis"
-      // lmorchard's old sandbox expo project
-      bundleIdentifier: "com.lmorchard.Didthis",
+      bundleIdentifier: "org.mozilla.Didthis",
       buildNumber: "4",
       config: {
         usesNonExemptEncryption: false,
@@ -20,16 +17,11 @@ export default ({ config }) => {
     },
     extra: {
       eas: {
-        // mozilla-ocho-h3y org project
         projectId: "d1216396-7cf9-4a41-9543-ce7e31e0529b",
-        // lmorchard's old sandbox expo project
-        // projectId: "bb2e6caf-a447-43b6-86cb-4ee477e6abc3",
       },
     },
     updates: {
       url: "https://u.expo.dev/d1216396-7cf9-4a41-9543-ce7e31e0529b",
-      // lmorchard's old sandbox expo project
-      // url: "https://u.expo.dev/bb2e6caf-a447-43b6-86cb-4ee477e6abc3",
     },
     runtimeVersion: {
       policy: "sdkVersion",
