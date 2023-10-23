f42aa4d511b7f779ffb49a8d3d046a3a732210f5
Les Orchard <lorchard@mozilla.com>
2023-10-23 16:40:11 -0700

    initial implementation of top nav bar with optional cancel and confirm

--- integrations/mobile-app/components/TopNav.stories.tsx
+++ integrations/mobile-app/components/TopNav.stories.tsx
@@ -1,12 +1,59 @@
 import type { Meta, StoryObj } from "@storybook/react-native";
 import { action } from "@storybook/addon-actions";
+import {
+  SafeAreaView,
+  StyleSheet,
+  View,
+  Text,
+  useColorScheme,
+} from "react-native";
+import {
+  NavigationContainer,
+  DefaultTheme,
+  DarkTheme,
+  useTheme,
+} from "@react-navigation/native";
 
 import TopNav, { TopNavProps } from "./TopNav";
 
 export default {
   title: "components/TopNav",
   component: TopNav,
-  decorators: [],
+  decorators: [
+    (Story) => {
+      const colorScheme = useColorScheme();
+      // const theme = colorScheme === "dark" ? DarkTheme : DefaultTheme;
+      const theme = DefaultTheme;
+      const { colors } = theme;
+
+      return (
+        <NavigationContainer theme={theme}>
+          <SafeAreaView
+            style={{
+              ...StyleSheet.absoluteFillObject,
+              flexDirection: "column",
+              marginTop: -47, // HACK: account for nav header asserting its own margin in storybook
+            }}
+          >
+            <Story />
+            <View
+              style={{ flexGrow: 1, padding: 16, justifyContent: "center" }}
+            >
+              <Text style={{ color: colors.text, padding: 48 }}>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat.
+              </Text>
+            </View>
+            <View style={{ flexGrow: 0, padding: 16 }}>
+              <Text style={{ color: colors.text }}>Bottom bar Duis aute irure dolor in reprehenderit</Text>
+            </View>
+          </SafeAreaView>
+        </NavigationContainer>
+      );
+    },
+  ],
 } satisfies Meta<typeof TopNav>;
 
 type Story = StoryObj<typeof TopNav>;
@@ -23,18 +70,35 @@ export const Default: Story = {
   args: baseArgs,
 };
 
-export const WithChevrons: Story = {
-  args: {...baseArgs, leftIsBack: true, rightIsForward: true }
+export const LeftOnly: Story = {
+  args: { ...baseArgs, rightLabel: undefined },
 };
 
-export const WithoutButtons: Story = {
-  args: {...baseArgs, onLeftPress: undefined, onRightPress: undefined }
+export const LeftDisabled: Story = {
+  args: { ...baseArgs, onLeftPress: undefined },
 };
 
-export const WithoutLeft: Story = {
-  args: {...baseArgs, onLeftPress: undefined }
+export const LeftIsBack: Story = {
+  args: { ...baseArgs, leftIsBack: true, rightIsForward: true },
 };
 
-export const WithoutRight: Story = {
-  args: {...baseArgs, onRightPress: undefined }
+export const LeftIsBackDisabled: Story = {
+  args: {
+    ...baseArgs,
+    leftIsBack: true,
+    leftLabel: "Back",
+    onLeftPress: undefined,
+  },
+};
+
+export const RightOnly: Story = {
+  args: { ...baseArgs, leftLabel: undefined },
+};
+
+export const TitleOnly: Story = {
+  args: { ...baseArgs, leftLabel: undefined, rightLabel: undefined },
+};
+
+export const RightDisabled: Story = {
+  args: { ...baseArgs, onRightPress: undefined },
 };
--- integrations/mobile-app/components/TopNav.tsx
+++ integrations/mobile-app/components/TopNav.tsx
@@ -1,132 +1,78 @@
 import {
-  View,
-  TouchableOpacity,
-  Button,
-  StyleSheet,
-  Text,
-  useColorScheme,
-} from "react-native";
-
-import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
-
-import { faChevronLeft } from "@fortawesome/free-solid-svg-icons/faChevronLeft";
-import { faChevronRight } from "@fortawesome/free-solid-svg-icons/faChevronRight";
+  Header,
+  HeaderBackButton,
+  HeaderTitle,
+} from "@react-navigation/elements";
+import * as React from "react";
+import { useTheme } from "@react-navigation/native";
 
 export type TopNavProps = {
   title?: string;
   leftIsBack?: boolean;
   leftLabel?: string;
   onLeftPress?: () => any;
-  rightIsForward?: boolean;
   rightLabel?: string;
   onRightPress?: () => any;
 };
 
+const noop = () => {};
+
 export default function TopNav({
   title,
   leftIsBack,
-  rightIsForward,
   leftLabel,
   rightLabel,
   onLeftPress,
   onRightPress,
 }: TopNavProps) {
-  const colorScheme = useColorScheme();
-  const styles = colorScheme == "dark" ? darkStyles : lightStyles;
+  const { colors } = useTheme();
 
   return (
-    <View style={styles.bar}>
-      {title && <Text style={styles.title}>{title}</Text>}
-      {onLeftPress && (
-        <TouchableOpacity style={styles.leftButton} onPress={onLeftPress}>
-            {leftIsBack && <FontAwesomeIcon icon={faChevronLeft} />}
-          <Text style={[styles.buttonText, styles.leftButton]}>
-            {leftLabel}
-          </Text>
-        </TouchableOpacity>
+    <Header
+      title={title}
+      headerShadowVisible={true}
+      headerLeft={() => (
+        <HeaderSideButton
+          {...{ onPress: onLeftPress, isBack: leftIsBack, label: leftLabel }}
+        />
       )}
-      {onRightPress && (
-        <TouchableOpacity style={styles.rightButton} onPress={onRightPress}>
-          <Text style={[styles.buttonText, styles.rightButton]}>
-            {rightLabel}
-          </Text>
-          {rightIsForward && <FontAwesomeIcon icon={faChevronRight} />}
-        </TouchableOpacity>
+      headerRight={() => (
+        <HeaderSideButton {...{ onPress: onRightPress, label: rightLabel }} />
       )}
-    </View>
+    />
   );
 }
 
-type Colors = {
-  fg: string;
-  fgMuted: string;
-  bg: string;
-};
-
-const buildThemeStyles = (colors: Colors) =>
-  StyleSheet.create({
-    bar: {
-      flexDirection: "row",
-      borderBottomWidth: 2,
-      justifyContent: "space-around",
-      paddingVertical: 24,
-      marginHorizontal: 0,
-      borderColor: colors.fgMuted,
-    },
-    buttonText: {
-      color: colors.fg,
-      fontSize: 17,
-    },
-    buttonEnabled: {},
-    buttonDisabled: {},
-    leftButton: {
-      flex: 1,
-      flexDirection: "row",
-      position: "absolute",
-      paddingHorizontal: 12,
-      left: 0,
-      textAlign: "left",
-      lineHeight: 48,
-    },
-    leftButtonText: {
-      textAlign: "right",
-      lineHeight: 48,
-    },
-    title: {
-      position: "absolute",
-      width: "100%",
-      lineHeight: 48,
-      left: 0,
-      paddingHorizontal: 12,
-      flexGrow: 1,
-      textAlign: "center",
-      fontSize: 17,
-      fontWeight: "bold",
-      color: colors.fg,
-    },
-    rightButton: {
-      flex: 1,
-      flexDirection: "row",
-      position: "absolute",
-      paddingHorizontal: 12,
-      right: 0,
-      lineHeight: 48,
-      textAlign: "right",
-    },
-    rightButtonText: {
-      textAlign: "right",
-      lineHeight: 48,
-    },
-  });
-
-const lightStyles = buildThemeStyles({
-  bg: "rgba(255, 255, 255, 1)",
-  fg: "rgba(0, 0, 0, 1.0)",
-  fgMuted: "rgba(0, 0, 0, 0.3)",
-});
-
-const darkStyles = buildThemeStyles({
-  bg: "rgba(0, 0, 0, 1)",
-  fg: "rgba(255, 255, 255, 1.0)",
-  fgMuted: "rgba(255, 255, 255, 0.3)",
-});
+function HeaderSideButton({
+  onPress,
+  isBack,
+  label,
+}: {
+  onPress: () => any;
+  isBack?: boolean;
+  label: string;
+}) {
+  const { colors } = useTheme();
+  let isDisabled = !onPress;
+  let style = {
+    opacity: isDisabled ? 0.3 : 1.0,
+  };
+  if (isBack) {
+    return (
+      <HeaderBackButton
+        style={style}
+        label={label || "Back"}
+        onPress={onPress}
+      />
+    );
+  } else if (label) {
+    return (
+      <HeaderTitle
+        onPress={isDisabled ? noop : onPress}
+        style={{ ...style, color: colors.primary, marginHorizontal: 22 }}
+      >
+        {label}
+      </HeaderTitle>
+    );
+  }
+}
