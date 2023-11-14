deafc6451c98313842eea84c4880da4db5f18501
Les Orchard <lorchard@mozilla.com>
2023-11-14 14:52:35 -0800

    issue #132: add a small delay on displaying the loader to reduce flashes during quick transitions

--- integrations/mobile-app/components/Loader.tsx
+++ integrations/mobile-app/components/Loader.tsx
@@ -1,6 +1,7 @@
 import { StyleSheet, SafeAreaView, StyleProp, ViewStyle } from "react-native";
 import LottieView from "lottie-react-native";
 import useAppShellHost from "../lib/appShellHost";
+import { useEffect, useRef, useState } from "react";
 
 export type LoaderProps = {
   style?: StyleProp<ViewStyle>;
@@ -27,9 +28,28 @@ export default function Loader({ style }: LoaderProps) {
   );
 }
 
-export function ConditionalLoader() {
+export type ConditionalLoaderProps = {
+  delay: number;
+};
+export function ConditionalLoader({ delay = 100 }) {
   const appShellHost = useAppShellHost();
   const loading = appShellHost.state?.loading;
-  if (!loading) return <></>;
+  const timerRef = useRef<ReturnType<typeof setTimeout> | undefined>();
+  const [delayPassed, setDelayPassed] = useState(false);
+
+  useEffect(() => {
+    setDelayPassed(false);
+    if (timerRef.current) clearTimeout(timerRef.current);
+
+    if (loading) {
+      timerRef.current = setTimeout(() => {
+        timerRef.current = undefined;
+        setDelayPassed(true);
+      }, delay);
+    }
+  }, [loading, delay, timerRef, setDelayPassed]);
+
+  const shouldShow = loading && delayPassed;
+  if (!shouldShow) return <></>;
   return <Loader style={{ backgroundColor: "rgba(0, 0, 0, 0.3)" }} />;
 }
