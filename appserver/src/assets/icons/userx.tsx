f7abbf13b764dd5d1a8401b8bb7f03934ffab2c1
katetaylormoz <kataylor@mozilla.com>
2023-12-07 14:45:43 -0500

    icon react components for list component

---
+++ appserver/src/assets/icons/userx.tsx
@@ -0,0 +1,19 @@
+function UserX() {
+    return (
+    <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <g clip-path="url(#clip0_3235_2163)">
+        <path d="M13.3333 17.5V15.8333C13.3333 14.9493 12.9821 14.1014 12.357 13.4763C11.7319 12.8512 10.8841 12.5 10 12.5H4.16667C3.28261 12.5 2.43476 12.8512 1.80964 13.4763C1.18452 14.1014 0.833332 14.9493 0.833332 15.8333V17.5" stroke="#BCBCBC" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+        <path d="M7.08333 9.16667C8.92428 9.16667 10.4167 7.67428 10.4167 5.83333C10.4167 3.99238 8.92428 2.5 7.08333 2.5C5.24238 2.5 3.75 3.99238 3.75 5.83333C3.75 7.67428 5.24238 9.16667 7.08333 9.16667Z" stroke="#BCBCBC" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+        <path d="M15 6.66602L19.1667 10.8327" stroke="#BCBCBC" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+        <path d="M19.1667 6.66602L15 10.8327" stroke="#BCBCBC" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+        </g>
+        <defs>
+        <clipPath id="clip0_3235_2163">
+        <rect width="20" height="20" fill="white"/>
+        </clipPath>
+        </defs>
+    </svg>
+);
+}
+
+export default UserX
