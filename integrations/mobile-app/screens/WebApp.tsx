72fd19c4da63d0d8372362b55dae426d9c0b65fa
Les Orchard <lorchard@mozilla.com>
2024-02-08 11:34:55 -0800

    issue #369: extract AppShellWebView component to share between two webview screens

--- integrations/mobile-app/screens/WebApp.tsx
+++ integrations/mobile-app/screens/WebApp.tsx
@@ -1,9 +1,7 @@
 import useAppShellHost from "../lib/appShellHost";
-import { useCallback, useEffect, useRef, useState } from "react";
-import WebView from "react-native-webview";
-import { SafeAreaView, StatusBar, StyleSheet, View } from "react-native";
-import { useFocusEffect } from "@react-navigation/native";
-import { StackScreenProps } from "@react-navigation/stack";
+import { useEffect } from "react";
+import { SafeAreaView, StyleSheet } from "react-native";
+import { StackNavigationProp, StackScreenProps } from "@react-navigation/stack";
 import { RootStackParamList } from "../App";
 import Config from "../lib/config";
 import Loader from "../components/Loader";
@@ -12,12 +10,10 @@ import { ConditionalTopNav } from "../components/TopNav";
 import { ConditionalBottomNav } from "../components/BottomNav";
 import AppShellHostAPI from "../lib/appShellHost/api";
 import { checkOnboarding } from "../lib/storage";
-import { ApiUser } from "../lib/types";
 import useDelay from "../lib/useDelay";
-import { useSafeAreaInsets } from "react-native-safe-area-context";
-import { WebViewTerminatedEvent } from "react-native-webview/lib/WebViewTypes";
+import AppShellWebView from "../components/AppShellWebView";
 
-const { siteBaseUrl, originWhitelist } = Config;
+const { siteBaseUrl } = Config;
 
 export type WebAppScreenRouteParams = {
   credential?: AppleAuthentication.AppleAuthenticationCredential;
@@ -32,14 +28,33 @@ export type WebAppScreenProps = {} & StackScreenProps<
 export default function WebAppScreen({ route, navigation }: WebAppScreenProps) {
   const { credential, resetWebViewAfter = 0 } = route.params || {};
 
+  return (
+    <AppShellWebView
+      source={{ uri: siteBaseUrl }}
+      resetWebViewAfter={resetWebViewAfter}
+      header={() => <ConditionalTopNav />}
+      footer={() => (
+        <>
+          <ConditionalBottomNav />
+          <WaitingForUserLoader />
+          <AuthAndOnboardingCheck {...{ credential, navigation }} />
+        </>
+      )}
+    />
+  );
+}
+
+// This empty component lives in the footer, so it has access to the app
+// shell host with populated webviewRef via useAppShellHost hook
+function AuthAndOnboardingCheck({
+  credential,
+  navigation,
+}: {
+  credential?: AppleAuthentication.AppleAuthenticationCredential;
+  navigation: StackNavigationProp<RootStackParamList, "WebApp", undefined>;
+}) {
   const appShellHost = useAppShellHost();
   const user = appShellHost.state?.user;
-
-  const webviewRef = useRef<WebView>(null);
-  useEffect(
-    () => appShellHost.setWebView(webviewRef.current),
-    [appShellHost, webviewRef.current]
-  );
 
   // Send the user off to onboarding once signed in, if necessary.
   useEffect(() => {
@@ -49,46 +64,11 @@ export default function WebAppScreen({ route, navigation }: WebAppScreenProps) {
     });
   }, [user, navigation]);
 
+  // Send Apple credential to web content, so we can trigger loginWithAppleId
+  // if we're not already logged in
   useSendAppleCredentialToWebContent(credential, appShellHost);
-  const webviewKey = useResettableWebviewKey(resetWebViewAfter);
 
-  const insets = useSafeAreaInsets();
-
-  // https://github.com/react-native-webview/react-native-webview/issues/2199
-  const onContentProcessDidTerminate = (syntheticEvent: WebViewTerminatedEvent) => {
-    const { nativeEvent } = syntheticEvent;
-    console.warn('Content process terminated, reloading', nativeEvent);
-    webviewRef.current.reload();
-  };
-
-  return (
-    <View
-      style={{
-        ...StyleSheet.absoluteFillObject,
-        // Skip top margin, taken care of by ConditionalTopNav
-        marginLeft: insets.left,
-        marginRight: insets.right,
-        marginBottom: insets.bottom,
-      }}
-    >
-      <StatusBar barStyle="dark-content" />
-      <ConditionalTopNav />
-      <WebView
-        key={webviewKey}
-        source={{ uri: siteBaseUrl }}
-        sharedCookiesEnabled={true}
-        originWhitelist={originWhitelist}
-        startInLoadingState={true}
-        renderLoading={() => <Loader />}
-        ref={webviewRef}
-        onMessage={appShellHost.onMessage}
-        onNavigationStateChange={appShellHost.onNavigationStateChange}
-        onContentProcessDidTerminate={onContentProcessDidTerminate}
-      />
-      <ConditionalBottomNav />
-      <WaitingForUserLoader />
-    </View>
-  );
+  return null;
 }
 
 /**
@@ -134,33 +114,4 @@ function useSendAppleCredentialToWebContent(
       appShellHost.postMessage("appleCredential", { credential });
     }
   }, [webContentReady, appShellHost, credential]);
-}
-
-/**
- * Produce a key which changes when the webview should be re-rendered & reset
- * on focus of the current screen.
- *
- * @param resetWebViewAfter value of Date.now() after which to reset the webview
- * @param appShellHost app shell host API used for reset
- * @returns string current react component key for webview
- */
-function useResettableWebviewKey(resetWebViewAfter: number): string {
-  // Resetting the webview is kind of a hack: We maintain a "generation"
-  // counter which, when incremented, forces a re-render of the component.
-  //
-  // This, in turn, discards state and forces a reset. Need to do this
-  // in cases like signin where a fresh session is expected after signout.
-  const [webviewGeneration, setWebviewGeneration] = useState(0);
-  const [lastResetWebView, setLastResetWebView] = useState(Date.now());
-
-  useFocusEffect(
-    useCallback(() => {
-      if (resetWebViewAfter !== 0 && resetWebViewAfter > lastResetWebView) {
-        setLastResetWebView(Date.now());
-        setWebviewGeneration((gen) => gen + 1);
-      }
-    }, [resetWebViewAfter])
-  );
-
-  return `webapp-webview-${webviewGeneration}`;
 }
--- integrations/mobile-app/screens/WebAppSignin.tsx
+++ integrations/mobile-app/screens/WebAppSignin.tsx
@@ -1,23 +1,10 @@
-import useAppShellHost from "../lib/appShellHost";
-import { useCallback, useEffect, useRef, useState } from "react";
-import WebView from "react-native-webview";
-import { SafeAreaView, StatusBar, StyleSheet, View } from "react-native";
-import { useFocusEffect } from "@react-navigation/native";
 import { StackScreenProps } from "@react-navigation/stack";
 import { RootStackParamList } from "../App";
 import Config from "../lib/config";
-import Loader from "../components/Loader";
-import * as AppleAuthentication from "expo-apple-authentication";
-import TopNav, { ConditionalTopNav } from "../components/TopNav";
-import { ConditionalBottomNav } from "../components/BottomNav";
-import AppShellHostAPI from "../lib/appShellHost/api";
-import { checkOnboarding } from "../lib/storage";
-import { ApiUser } from "../lib/types";
-import useDelay from "../lib/useDelay";
-import { useSafeAreaInsets } from "react-native-safe-area-context";
-import { WebViewTerminatedEvent } from "react-native-webview/lib/WebViewTypes";
+import TopNav from "../components/TopNav";
+import AppShellWebView from "../components/AppShellWebView";
 
-const { siteBaseUrl, originWhitelist } = Config;
+const { siteBaseUrl } = Config;
 
 export type RouteParams = {
   resetWebViewAfter?: number;
@@ -28,85 +15,27 @@ export type WebAppSignInScreenProps = {} & StackScreenProps<
   "WebApp"
 >;
 
-export default function WebAppSigninScreen({ route, navigation }: WebAppSignInScreenProps) {
+export default function WebAppSigninScreen({
+  route,
+  navigation,
+}: WebAppSignInScreenProps) {
   const { resetWebViewAfter = 0 } = route.params || {};
 
-  const appShellHost = useAppShellHost();
-
-  const webviewRef = useRef<WebView>(null);
-  useEffect(
-    () => appShellHost.setWebView(webviewRef.current),
-    [appShellHost, webviewRef.current]
-  );
-
-  const webviewKey = useResettableWebviewKey(resetWebViewAfter);
-
-  const insets = useSafeAreaInsets();
-
-  // https://github.com/react-native-webview/react-native-webview/issues/2199
-  const onContentProcessDidTerminate = (syntheticEvent: WebViewTerminatedEvent) => {
-    const { nativeEvent } = syntheticEvent;
-    console.warn('Content process terminated, reloading', nativeEvent);
-    webviewRef.current.reload();
-  };
-
   const onCancelSignin = () => {
-    navigation.navigate("Signin")
+    navigation.navigate("Signin");
   };
 
   return (
-    <View
-      style={{
-        ...StyleSheet.absoluteFillObject,
-        // Skip top margin, taken care of by ConditionalTopNav
-        marginLeft: insets.left,
-        marginRight: insets.right,
-        marginBottom: insets.bottom,
-      }}
-    >
-      <StatusBar barStyle="dark-content" />
-      <TopNav title="Sign in with Email" leftIsBack onLeftPress={onCancelSignin} />
-      <WebView
-        key={webviewKey}
-        source={{ uri: `${siteBaseUrl}/signinWithEmail` }}
-        sharedCookiesEnabled={true}
-        originWhitelist={originWhitelist}
-        startInLoadingState={true}
-        renderLoading={() => <Loader />}
-        ref={webviewRef}
-        onMessage={appShellHost.onMessage}
-        onNavigationStateChange={appShellHost.onNavigationStateChange}
-        onContentProcessDidTerminate={onContentProcessDidTerminate}
-      />
-    </View>
+    <AppShellWebView
+      source={{ uri: `${siteBaseUrl}/signinWithEmail` }}
+      resetWebViewAfter={resetWebViewAfter}
+      header={() => (
+        <TopNav
+          title="Sign in with Email"
+          leftIsBack
+          onLeftPress={onCancelSignin}
+        />
+      )}
+    />
   );
-}
-
-/**
- * Produce a key which changes when the webview should be re-rendered & reset
- * on focus of the current screen.
- *
- * @param resetWebViewAfter value of Date.now() after which to reset the webview
- * @param appShellHost app shell host API used for reset
- * @returns string current react component key for webview
- */
-function useResettableWebviewKey(resetWebViewAfter: number): string {
-  // Resetting the webview is kind of a hack: We maintain a "generation"
-  // counter which, when incremented, forces a re-render of the component.
-  //
-  // This, in turn, discards state and forces a reset. Need to do this
-  // in cases like signin where a fresh session is expected after signout.
-  const [webviewGeneration, setWebviewGeneration] = useState(0);
-  const [lastResetWebView, setLastResetWebView] = useState(Date.now());
-
-  useFocusEffect(
-    useCallback(() => {
-      if (resetWebViewAfter !== 0 && resetWebViewAfter > lastResetWebView) {
-        setLastResetWebView(Date.now());
-        setWebviewGeneration((gen) => gen + 1);
-      }
-    }, [resetWebViewAfter])
-  );
-
-  return `webapp-webview-${webviewGeneration}`;
 }
