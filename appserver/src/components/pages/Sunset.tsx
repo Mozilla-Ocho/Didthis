b17692b99d62fb16b08ed91b546ca0d89abe287b
Josh Whiting <joshwhiting@gmail.com>
2024-10-16 12:30:43 -0700

    sunset announcement copy updates

--- appserver/src/components/pages/Sunset.tsx
+++ appserver/src/components/pages/Sunset.tsx
@@ -17,9 +17,10 @@ const Sunset = () => {
     onLeftPress: () => store.goBack(),
   })
 
-  const settingsLink = 
-  store.user ? (
-    <Link href={PathBuilder.userEdit(store.user.systemSlug)}>account details</Link>
+  const settingsLink = store.user ? (
+    <Link href={PathBuilder.userEdit(store.user.systemSlug)}>
+      account details
+    </Link>
   ) : (
     <span>account details</span>
   )
@@ -33,7 +34,7 @@ const Sunset = () => {
           <p>October 15, 2024</p>
           <p>Dear Didthis Community,</p>
           <p>
-            We’re writing to inform you that Didthis will be shutting down on
+            We are sad to announce that Didthis will be shutting down on
             November 15th. This includes the Didthis app, website, and Discord.
             We understand that this may come as unexpected news, and we’re
             incredibly grateful for the time you’ve spent with us sharing your
@@ -41,38 +42,52 @@ const Sunset = () => {
           </p>
 
           <p>
-            We’ve been inspired by the wonderful projects you shared—whether it
-            was documenting hikes through breathtaking national forests,
+            We’ve been inspired by the wonderful projects you shared — whether
+            it was documenting hikes through breathtaking national forests,
             crocheting everything under the sun, or even building your own
             furniture from scratch. In a world increasingly shaped by generative
-            AI slop and the impersonal nature of technology, your creativity is
-            a refreshing reminder of what truly connects us. It’s been a breath
-            of fresh air, showing us that the things that bring us joy are still
+            AI and the impersonal nature of technology, your creativity is a
+            refreshing reminder of what truly connects us. It’s been a breath of
+            fresh air, showing us that the things that bring us joy are still
             rooted in genuine human connection.
           </p>
 
           <p>
             We believe the vision for Didthis - a healthy space for hobbyists to
             capture and share their projects - is valuable and important.
-            However, we weren’t able to find a path forward for Didthis as a
-            viable growing product.
+            However, we weren’t able to find a path forward for Didthis in the
+            Mozilla ecosystem at this time.
           </p>
 
           <p>
             As we prepare to close this chapter, we want to ensure that you have
-            time to export your data before the shutdown. <strong>To download a copy of
-            your data, ensure you are logged in and then simply navigate to{' '}
-            {settingsLink}, find the “Account data export” section at the bottom of
-            the page, and click “Export account.”</strong> Your data should be packaged
-            up shortly and be made available as a link to a ZIP file on that
-            same page.
+            time to export your data before the shutdown. To download a copy of
+            your data, simply navigate to {settingsLink}, find the “Account data
+            export” section at the bottom of this page, and click “Export
+            account”. Your data will then be packaged up and made available as a
+            link to a ZIP file on that same page.
+          </p>
+
+          <p>This zip will include:</p>
+          <ul>
+            <li>All your images</li>
+            <li>All your projects and project updates in HTML + CSS</li>
+            <li>
+              Your profile and project data, public & private, in JSON form
+            </li>
+            <li>The Didthis logo in SVG form</li>
+          </ul>
+          <p>
+            Please note that this data is provided in a raw format (JSON and
+            HTML) which can be opened directly with your web-browser of choice,
+            but may require technical skills to use for any other purpose.{' '}
           </p>
 
           <p>
             We will also be open sourcing the code behind Didthis. Mozilla is a
             leader in the open source community, and sharing our work is one of
             our most closely held values. We hope this code will be interesting
-            or helpful to others.{' '}
+            or helpful to others.
           </p>
 
           <p>
